9bdc78d74041268c34de9359bce849ca63844e67
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-08-17 01:46:16 +0300

    refactor: clean up vite.config.js by removing unused environments configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: "0.0.0.0",
     port: 5173,
-  }
-  ,
-  environments: {
-    production: {
-      apiUrl: import.meta.env.VITE_PROD_API_URL,
-    },
-    development: {
-      apiUrl: import.meta.env.VITE_DEV_API_URL,
-    },
-  }
-})
+  },
+});
